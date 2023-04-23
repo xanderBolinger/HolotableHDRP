64ea2ccbefaa067c9e1f139d7091f093d3dad847
xander@cuttingedgemachining.onmicrosoft.com <xanderleebolinger@gmail.com>
2023-04-22 20:12:05 -0600

    c

---
+++ Assets/Hex Map/HexMap.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexMap : MonoBehaviour
+{
+
+    public GameObject hexPrefab;
+    public int mapWidth = 25;
+    public int mapHeight = 12;
+    
+    float xSpacing = 0.2f;
+    float zSpacing = 0.16f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateTileMap();
+    }
+
+    void CreateTileMap() {
+
+
+        for (int x = 0; x <= mapWidth; x++) {
+            for (int z = 0; z <= mapHeight; z++) {
+
+                GameObject hex = Instantiate(hexPrefab);
+
+                if (z % 2 == 0) {
+                    hex.transform.position = new Vector3(x * xSpacing, 0, z*zSpacing);
+                }
+
+            
+            }
+        }
+
+    }
+}
