c313cc7aae3499e0ba4b158a3bdf6941034b57fd
xander@cuttingedgemachining.onmicrosoft.com <xanderleebolinger@gmail.com>
2023-10-24 13:19:31 -0600

    c

---
+++ Assets/Scripts/AirBattle/Aircraft.cs
@@ -0,0 +1,18 @@
+
+public class Aircraft
+{
+    string _callsign;
+    string _aircraftName;
+    AircraftMovementData _movementData;
+
+
+    public string callsign { get { return _callsign; } }
+    public string aircraftName { get { return _aircraftName; } }
+
+    
+
+
+
+
+
+}
