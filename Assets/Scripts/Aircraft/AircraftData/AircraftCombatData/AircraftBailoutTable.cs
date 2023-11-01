b24fb67da65fd5d14d5a0da12e1df56149b63a8e
xanderBolinger <52334326+xanderBolinger@users.noreply.github.com>
2023-11-01 11:21:16 -0600

    c

--- Assets/Scripts/Aircraft/AircraftData/AircraftCombatData/AircraftBailoutTable.cs
+++ Assets/Scripts/Aircraft/AircraftData/AircraftCombatData/AircraftBailoutTable.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AircraftBailoutTable : MonoBehaviour
+public class AircraftBailoutTable
 {
 
     public enum BailoutResult { 
