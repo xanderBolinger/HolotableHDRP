2e54f0602c0947e9adcb4863bcb81066bcabadd1
xander@cuttingedgemachining.onmicrosoft.com <xanderleebolinger@gmail.com>
2023-11-02 19:23:27 -0600

    c

--- Assets/Scripts/SmoothCameraMotor.cs
+++ Assets/Scripts/SmoothCameraMotor.cs
@@ -5,28 +5,27 @@ using UnityEngine;
 
 public class SmoothCameraMotor : MonoBehaviour
 {
-    [SerializeField, Range(-20f, -90f)] float bottomAngleLimit;
-    [SerializeField, Range(70f, 90f)] float topAngleLimit;
+    [SerializeField, Range(-1f, -90f)] float _topAngleLimit;
+    [SerializeField, Range(1f, 90f)] float _bottomAngleLimit;
     [SerializeField, Range(0.01f, 1f)] float shiftMoveSpeed;
     [SerializeField, Range(1f, 5f)] float _moveSpeed;
+    [SerializeField, Range(1f, 10f)] float _moveAcceleration;
+    [SerializeField, Range(1f, 10f)] float _moveDeceleration;
 
     [SerializeField]
     private float _lookSensitivity = 3.0f;
     [SerializeField]
     private float _scrollSpeed = 20.0f;
 
+    [SerializeField]
+    private float _smoothTimeMovement = 0.2f;
+
+    [SerializeField, Range(0.5f, 0.9f)]
+    private float _smoothTime = 0.2f;
+
     private float _rotationY;
     private float _rotationX;
     private Transform _transform;
-    private float _nextDistanceFromTarget;
-
-    [SerializeField]
-    private float _distanceFromTarget = 10.0f;
-
-    [SerializeField]
-    private float _minDistanceFromTarget = 5.0f;
-    [SerializeField]
-    private float _maxDistanceFromTarget = 5.0f;
 
     private Vector3 _currentRotation;
     private Vector3 _smoothVelocityHeight = Vector3.zero;
@@ -34,16 +33,9 @@ public class SmoothCameraMotor : MonoBehaviour
 
     private Vector3 _nextRotation = Vector3.zero;
 
-
-    [SerializeField]
-    private float _smoothTimeMovement = 0.2f;
-    [SerializeField, Range(0.5f,0.9f)]
-    private float _smoothTime = 0.2f;
-
     private void Awake()
     {
         _transform = transform;
-        _nextDistanceFromTarget = _distanceFromTarget;
     }
 
     void Update()
@@ -60,28 +52,62 @@ public class SmoothCameraMotor : MonoBehaviour
 
     private void Move()
     {
-        var originalY = _transform.position.y;
-        var movement = _transform.position;
-
-        if (Input.GetKey(KeyCode.W))
-            movement += _transform.forward * Time.deltaTime * _moveSpeed;
-
-        if (Input.GetKey(KeyCode.S))
-            movement -= _transform.forward * Time.deltaTime * _moveSpeed;
-
-        if (Input.GetKey(KeyCode.A))
-            movement -= _transform.right * Time.deltaTime * _moveSpeed;
-
-        if (Input.GetKey(KeyCode.D))
-            movement += _transform.right * Time.deltaTime * _moveSpeed;
-
-        float originalMagnitude = movement.magnitude;
+        var pos = _transform.position;
         
-        movement.y = originalY;
-        //movement = movement.normalized * originalMagnitude;
+        Quaternion rotation = Quaternion.Euler(0, _transform.eulerAngles.y, 0);
+        Vector3 localDirection = Vector3.forward;
+        Vector3 forward = rotation * localDirection;
+
+        var zPos = forward * MoveForwardAndBack();
+        var xPos = _transform.right * MoveSideToSide();
+
+        var movement = zPos + xPos;
+        movement.y = pos.y;
 
         _transform.position = movement;
     }
+
+    float currentSpeed;
+    float currentSpeedForward;
+
+    float MoveForwardAndBack()
+    {
+        if ((Input.GetKey(KeyCode.W)) && (currentSpeedForward < _moveSpeed))
+            currentSpeedForward = currentSpeedForward - _moveAcceleration * Time.deltaTime;
+        else if ((Input.GetKey(KeyCode.S)) && (currentSpeedForward > -_moveSpeed))
+            currentSpeedForward = currentSpeedForward + _moveAcceleration * Time.deltaTime;
+        else
+        {
+            if (currentSpeedForward > _moveDeceleration * Time.deltaTime)
+                currentSpeedForward = currentSpeedForward - _moveDeceleration * Time.deltaTime;
+            else if (currentSpeedForward < -_moveDeceleration * Time.deltaTime)
+                currentSpeedForward = currentSpeedForward + _moveDeceleration * Time.deltaTime;
+            else
+                currentSpeedForward = 0;
+        }
+
+        return _transform.position.z + currentSpeedForward * Time.deltaTime;
+    }
+
+    float MoveSideToSide()
+    {
+        if ((Input.GetKey(KeyCode.A)) && (currentSpeed < _moveSpeed))
+            currentSpeed = currentSpeed - _moveAcceleration * Time.deltaTime;
+        else if ((Input.GetKey(KeyCode.D)) && (currentSpeed > -_moveSpeed))
+            currentSpeed = currentSpeed + _moveAcceleration * Time.deltaTime;
+        else
+        {
+            if (currentSpeed > _moveDeceleration * Time.deltaTime)
+                currentSpeed = currentSpeed - _moveDeceleration * Time.deltaTime;
+            else if (currentSpeed < -_moveDeceleration * Time.deltaTime)
+                currentSpeed = currentSpeed + _moveDeceleration * Time.deltaTime;
+            else
+                currentSpeed = 0;
+        }
+
+        return _transform.position.x + currentSpeed * Time.deltaTime;
+    }
+
 
     void SetRotation() {
         if (!Input.GetMouseButton(1))
@@ -93,7 +119,7 @@ public class SmoothCameraMotor : MonoBehaviour
         _rotationY += mouseX;
         _rotationX += -mouseY;
 
-        _rotationX = Mathf.Clamp(_rotationX, bottomAngleLimit, topAngleLimit);
+        _rotationX = Mathf.Clamp(_rotationX, _topAngleLimit, _bottomAngleLimit);
 
         _nextRotation = new Vector3(_rotationX, _rotationY);
     }
@@ -109,18 +135,6 @@ public class SmoothCameraMotor : MonoBehaviour
         Vector3 camera = new Vector3(pos.x, pos.y + Input.GetAxis("Mouse ScrollWheel") * _scrollSpeed, pos.z);
 
         _transform.position = Vector3.SmoothDamp(pos, camera, ref _smoothVelocityHeight, _smoothTime);
-    }
-
-    void UpdateDistanceToTarget()
-    {
-        _nextDistanceFromTarget += -Input.GetAxis("Mouse ScrollWheel") * _scrollSpeed;
-        if (_nextDistanceFromTarget < _minDistanceFromTarget)
-            _nextDistanceFromTarget = _minDistanceFromTarget;
-        else if (_nextDistanceFromTarget > _maxDistanceFromTarget)
-            _nextDistanceFromTarget = _maxDistanceFromTarget;
-
-        _distanceFromTarget = Mathf.Lerp(_distanceFromTarget,
-            _nextDistanceFromTarget, 1 * Time.deltaTime);
     }
 
 }
