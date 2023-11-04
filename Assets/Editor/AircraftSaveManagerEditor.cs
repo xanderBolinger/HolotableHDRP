fe4f250e434f87bdf77200ead92bcbfaa0852b1a
xander@cuttingedgemachining.onmicrosoft.com <xanderleebolinger@gmail.com>
2023-11-04 15:47:53 -0600

    c

---
+++ Assets/Editor/AircraftSaveManagerEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(AircraftSaveManager))]
+public class AircraftSaveManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        AircraftSaveManager sm = (AircraftSaveManager)target;
+
+        if (GUILayout.Button("Save"))
+        {
+            sm.SaveAircraft();
+        }
+
+        if (GUILayout.Button("Load"))
+        {
+            sm.LoadAircraft();
+        }
+
+    }
+
+
+}
