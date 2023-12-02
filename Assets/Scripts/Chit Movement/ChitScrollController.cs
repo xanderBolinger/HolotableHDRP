ae51aadffaf4a4e148726fda2990d45a55493434
xander@cuttingedgemachining.onmicrosoft.com <xanderleebolinger@gmail.com>
2023-12-02 13:38:10 -0700

    c

--- Assets/Scripts/Chit Movement/ChitScrollController.cs
+++ Assets/Scripts/Chit Movement/ChitScrollController.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class ChitScrollController : MonoBehaviour
 {
-    [SerializeField]
+    [SerializeField, Range(1f, 360f)]
     float rotateSpeed;
-    [SerializeField]
+    [SerializeField, Range(1f, 5f)]
     float scaleSpeed;
-    [SerializeField]
+    [SerializeField, Range(0.1f, 1f)]
     float changeHeightSpeed;
 
     void Update()
@@ -19,6 +19,7 @@ public class ChitScrollController : MonoBehaviour
         {
             // Handle the scroll input (e.g., zoom in/out)
             Debug.Log("Scroll Direction: " + scrollDirection);
+            HandleScrollChit(scrollDirection);
         }
     }
 
@@ -27,12 +28,12 @@ public class ChitScrollController : MonoBehaviour
             return;
 
         foreach (var chit in ChitManager.instance.selectedChits) {
-
-            if (Input.GetKeyDown(KeyCode.LeftShift))
+           
+            if (Input.GetKey(KeyCode.LeftShift))
             {
                 IncreaseHeight(chit, direction);
             }
-            else if (Input.GetKeyDown(KeyCode.LeftControl))
+            else if (Input.GetKey(KeyCode.LeftControl))
             {
                 Scale(chit, direction);
             }
@@ -42,26 +43,38 @@ public class ChitScrollController : MonoBehaviour
         
         }
 
-
     }
 
     private void IncreaseHeight(Chit chit, float direction) {
         Debug.Log("Change height");
         if (direction > 0)
         {
-            chit.elevatedPosition.y += chit.boxCollider.size.y;
+            chit.elevatedPosition.y += chit.boxCollider.size.y * changeHeightSpeed;
         }
         else {
-            chit.elevatedPosition.y -= chit.boxCollider.size.y;
+            chit.elevatedPosition.y -= chit.boxCollider.size.y * changeHeightSpeed;
         }
     }
 
-    private void Rotate(Chit chit, float direction) { 
-    
+    private void Rotate(Chit chit, float direction)
+    {
+        Debug.Log("Rotate");
+        float rotationAmount = rotateSpeed * direction;
+        chit.transform.Rotate(Vector3.up, rotationAmount);
     }
 
-    private void Scale(Chit chit, float direction) { 
-    
+    private void Scale(Chit chit, float direction)
+    {
+        Debug.Log("Scale");
+        float scaleAmount = scaleSpeed * direction * Time.deltaTime;
+
+        Vector3 newScale = chit.transform.localScale + new Vector3(scaleAmount, scaleAmount, scaleAmount);
+
+        // Ensure the new scale is within a reasonable range
+        newScale = Vector3.Max(newScale, new Vector3(0.1f, 0.1f, 0.1f));
+        newScale = Vector3.Min(newScale, new Vector3(3f, 3f, 3f));
+
+        chit.transform.localScale = newScale;
     }
 
     private float GetScrollInput()
